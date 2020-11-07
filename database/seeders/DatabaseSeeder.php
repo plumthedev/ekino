5bebd7995d63cad52cd8c62e4068601529eeac31
Kacper Pruszyński <kacper.pruszynski@movecloser.pl>
2020-11-07 14:34:37 +0100

    Use user seeder if is not production.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,15 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		if (!app()->environment('production')) {
+			$this->call(UserSeeder::class);
+		}
+	}
 }
