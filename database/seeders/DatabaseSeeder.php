60b0a6ea93624ef07be006ad197dd576f7b29fb1
Kacper Pruszyński <kacper.pruszynski@movecloser.pl>
2020-11-07 22:35:01 +0100

    Refactor database seeder to use strategy.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,12 +12,64 @@ class DatabaseSeeder extends Seeder
 	 *
 	 * @return void
 	 */
-	public function run()
+	public function run(): void
+	{
+		$this->runBaseSeeding();
+		$this->runEnvironmentSeeding();
+	}
+
+	/**
+	 * Run base seeding.
+	 * Create system users.
+	 *
+	 * @return void
+	 */
+	public function runBaseSeeding(): void
 	{
 		$this->call(BaseSeeder::class);
+	}
 
-		if (!app()->environment('production')) {
-			$this->call(UsersSeeder::class);
+	/**
+	 * Run seeding method based on current environment.
+	 *
+	 * @return void
+	 */
+	public function runEnvironmentSeeding(): void
+	{
+		$seedingMethodName = $this->composeEnvironmentSeedingMethodName();
+
+		if (!is_string($seedingMethodName) || !method_exists($this, $seedingMethodName)) {
+			return;
 		}
+
+		$this->$seedingMethodName();
+	}
+
+	/**
+	 * Run local environment seeding.
+	 *
+	 * @return void
+	 */
+	public function runLocalSeeding(): void
+	{
+		$this->call(UsersSeeder::class);
+		$this->call(MoviesSeeder::class);
+	}
+
+	/**
+	 * Compose environment seeding method name.
+	 *
+	 * @return string|null
+	 */
+	protected function composeEnvironmentSeedingMethodName(): ?string
+	{
+		$environment = app()->environment();
+
+		if (!is_string($environment)) {
+			return null;
+		}
+
+		$environment = Str::studly($environment);
+		return "run{$environment}Seeding";
 	}
 }
