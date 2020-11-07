b969f10c9fa90a59bd08a4d1dc4ddd9ff14bcc7d
Kacper Pruszyński <kacper.pruszynski@movecloser.pl>
2020-11-07 14:33:36 +0100

    Modify user factory.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,32 +2,66 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = User::class;
+	/**
+	 * Default password for the user.
+	 *
+	 * @var string
+	 */
+	const DEFAULT_PASSWORD = 'password';
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
-        return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
-    }
+	/**
+	 * The name of the factory's corresponding model.
+	 *
+	 * @var string
+	 */
+	protected $model = \App\Models\User::class;
+
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array
+	 */
+	public function definition()
+	{
+		return [
+			'first_name'        => $this->faker->firstName,
+			'last_name'         => $this->faker->lastName,
+			'email'             => $this->faker->unique()->safeEmail,
+			'password'          => self::DEFAULT_PASSWORD,
+			'verified_at' => now(),
+		];
+	}
+
+	/**
+	 * Not verified user state.
+	 *
+	 * @return \Database\Factories\UserFactory
+	 */
+	public function notVerified(): UserFactory
+	{
+		return $this->state(function (){
+			return [
+				'verified_at' => null,
+			];
+		});
+	}
+
+	/**
+	 * Nameless user state.
+	 *
+	 * @return \Database\Factories\UserFactory
+	 */
+	public function nameless(): UserFactory
+	{
+		return $this->state(function (){
+			return [
+				'first_name' => null,
+				'last_name'  => null,
+			];
+		});
+	}
 }
