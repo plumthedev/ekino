2cf3f5b125bd601c84f8e395ee01eb7c0fb8f877
Kacper Pruszyński <kacper.pruszynski@movecloser.pl>
2020-11-07 14:32:34 +0100

    Modify users table migration.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,31 +6,38 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Users table name.
+	 *
+	 * @var string
+	 */
+	protected $tableName = 'users';
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists($this->tableName);
+	}
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up(): void
+	{
+		Schema::create($this->tableName, function (Blueprint $table) {
+			$table->id();
+			$table->string('first_name')->nullable();
+			$table->string('last_name')->nullable();
+			$table->string('email')->unique();
+			$table->string('password');
+			$table->timestamp('verified_at')->nullable();
+			$table->timestamps();
+		});
+	}
 }
