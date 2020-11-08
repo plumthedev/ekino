f6c93bb04cf07478ede9265d75d1e0c228ca69db
Kacper Pruszyński <kacper.pruszynski@movecloser.pl>
2020-11-08 13:02:06 +0100

    Replace eloquent factory with cinematography factory in movies factory.

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -2,10 +2,7 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
-
-class MovieFactory extends Factory
+class MovieFactory extends CinematographyFactory
 {
 	/**
 	 * The name of the factory's corresponding model.
@@ -23,7 +20,7 @@ class MovieFactory extends Factory
 	{
 		return [
 			'title'       => $this->generateTitle(),
-			'content'     => $this->faker->realText(1500),
+			'content'     => $this->generateContent(),
 			'duration'    => $this->generateDuration(),
 			'rating'      => $this->generateRating(),
 			'meta'        => $this->generateMeta(),
@@ -108,79 +105,5 @@ class MovieFactory extends Factory
 				]),
 			];
 		});
-	}
-
-	/**
-	 * Generate movie duration.
-	 *
-	 * @return string
-	 */
-	protected function generateDuration(): string
-	{
-		$duration = now()->setTime(
-			$this->faker->numberBetween(1, 2),
-			$this->faker->randomElement([15, 30, 45, 50, 55]),
-			0
-		);
-
-		return $duration->format('H:i:s');
-	}
-
-	/**
-	 * Generate movie meta.
-	 * Pass additional arguments to overwrite generated.
-	 *
-	 * @param array $attributes
-	 *
-	 * @return array
-	 */
-	protected function generateMeta(array $attributes = []): array
-	{
-		return array_merge([
-			'is_premiere'    => false,
-			'is_recommended' => false,
-			'trailer_url'    => $this->faker->url,
-		], $attributes);
-	}
-
-	/**
-	 * Generate movie produced at date.
-	 * Pass start date to limit date interval in past.
-	 *
-	 * @param string $startDate
-	 *
-	 * @return string
-	 */
-	protected function generateProducedAt(string $startDate = '-30 years'): string
-	{
-		return $this->faker->dateTimeBetween($startDate)->format('Y-m-d');
-	}
-
-	/**
-	 * * Generate movie rating.
-	 *
-	 * @param int $min
-	 * @param int $max
-	 *
-	 * @return float
-	 */
-	protected function generateRating(int $min = 1, int $max = 5): float
-	{
-		return $this->faker->randomFloat(2, $min, $max);
-	}
-
-	/**
-	 * Generate movie title.
-	 *
-	 * @return string
-	 */
-	protected function generateTitle(): string
-	{
-		$title = $this->faker->realText();
-		$title = Str::lower($title);
-		$title = Str::words($title, $this->faker->numberBetween(1, 4), null);
-		$title = Str::ucfirst($title);
-
-		return $title;
 	}
 }
