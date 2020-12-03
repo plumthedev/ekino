2bf5012a0fcf55b57d1c823b8115d8f08bb84f88
plumthedev <kacper.pruszynski99@gmail.com>
2020-12-04 00:23:28 +0100

    Add faker on actors seeder.

--- database/seeders/ActorsSeeder.php
+++ database/seeders/ActorsSeeder.php
@@ -6,106 +6,129 @@ use App\Models\Actor;
 use App\Models\Cinematography;
 use App\Services\ImageGenerator\Contracts\Service as ImageGenerator;
 use Database\Factories\ActorFactory;
+use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
 class ActorsSeeder extends Seeder
 {
-	/**
-	 * Image generator.
-	 *
-	 * @var \App\Services\ImageGenerator\Contracts\Service
-	 */
-	protected $imageGenerator;
+    /**
+     * Image generator.
+     *
+     * @var \App\Services\ImageGenerator\Contracts\Service
+     */
+    protected $imageGenerator;
 
-	/**
-	 * Seeder constructor.
-	 *
-	 * @param \App\Services\ImageGenerator\Contracts\Service $imageGenerator
-	 */
-	public function __construct(ImageGenerator $imageGenerator)
-	{
-		$this->imageGenerator = $imageGenerator;
-	}
+    /**
+     * Faker generator.
+     *
+     * @var \Faker\Generator
+     */
+    protected $faker;
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		$this->createActors();
-		$this->createActorsAvatar();
-		$this->assignActorsToCinematographies();
-	}
+    /**
+     * Seeder constructor.
+     *
+     * @param \App\Services\ImageGenerator\Contracts\Service $imageGenerator
+     * @param \Faker\Generator                               $faker
+     */
+    public function __construct(ImageGenerator $imageGenerator, Faker $faker)
+    {
+        $this->imageGenerator = $imageGenerator;
+        $this->faker = $faker;
+    }
 
-	/**
-	 * For all actors in database
-	 * assign some cinematographic performs.
-	 *
-	 * @return void
-	 */
-	protected function assignActorsToCinematographies(): void
-	{
-		foreach (Actor::all() as $actor) {
-			foreach ($this->findSomeCinematographies() as $cinematography) {
-				$actor->cinematographies()->attach(
-					$cinematography->id,
-					$this->actorFactory()->performs()
-				);
-			}
-		}
-	}
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $this->createActors();
+        $this->assignActorsToCinematographies();
+        $this->createActorsMedia();
+    }
 
-	/**
-	 * Create actors.
-	 *
-	 * @return void
-	 */
-	protected function createActors(): void
-	{
-		$this->actorFactory()->count(10)->create();
-	}
+    /**
+     * For all actors in database
+     * assign some cinematographic performs.
+     *
+     * @return void
+     */
+    protected function assignActorsToCinematographies(): void
+    {
+        foreach (Actor::all() as $actor) {
+            foreach ($this->findSomeCinematographies() as $cinematography) {
+                $actor->cinematographies()->attach(
+                    $cinematography->id,
+                    $this->actorFactory()->performs()
+                );
+            }
+        }
+    }
 
-	/**
-	 * Create actors avatars.
-	 *
-	 * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded
-	 * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\DiskDoesNotExist
-	 * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\FileDoesNotExist
-	 * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\FileIsTooBig
-	 */
-	protected function createActorsAvatar(): void
-	{
-		foreach (Actor::all() as $actor) {
-			$avatar = $this->imageGenerator->person()->getImage();
+    /**
+     * Create actors.
+     *
+     * @return void
+     */
+    protected function createActors(): void
+    {
+        $this->actorFactory()->count(10)->create();
+    }
 
-			$actor
-				->addMedia($avatar)
-				->toMediaCollection(Actor::MEDIA_COLLECTION_AVATAR);
-		}
-	}
+    /**
+     * Create actor avatars.
+     *
+     * @param \App\Models\Actor $actor
+     *
+     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\DiskDoesNotExist
+     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\FileDoesNotExist
+     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\FileIsTooBig
+     */
+    protected function createActorAvatar(Actor $actor): void
+    {
+        $avatar = $this->imageGenerator->person()->getImage();
 
-	/**
-	 * Find some random cinematographies.
-	 *
-	 * @return \Illuminate\Database\Eloquent\Collection
-	 */
-	protected function findSomeCinematographies(): Collection
-	{
-		return Cinematography::inRandomOrder()->take(
-			mt_rand(2, 4)
-		)->get();
-	}
+        $actor
+            ->addMedia($avatar)
+            ->toMediaCollection(Actor::MEDIA_COLLECTION_AVATAR);
+    }
 
-	/**
-	 * Actor factory.
-	 *
-	 * @return \Database\Factories\ActorFactory
-	 */
-	protected function actorFactory(): ActorFactory
-	{
-		return Actor::factory();
-	}
+    /**
+     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\DiskDoesNotExist
+     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\FileDoesNotExist
+     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\FileIsTooBig
+     */
+    protected function createActorsMedia(): void
+    {
+        foreach (Actor::all() as $actor) {
+            if ($this->faker->boolean(75)) {
+                $this->createActorAvatar($actor);
+            }
+        }
+    }
+
+    /**
+     * Find some random cinematographies.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    protected function findSomeCinematographies(): Collection
+    {
+        return Cinematography::inRandomOrder()->take(
+            mt_rand(2, 4)
+        )->get();
+    }
+
+    /**
+     * Actor factory.
+     *
+     * @return \Database\Factories\ActorFactory
+     */
+    protected function actorFactory(): ActorFactory
+    {
+        return Actor::factory();
+    }
 }
