523ab1522400972269ef5ed826dc5b5b9c710420
plumthedev <kacper.pruszynski99@gmail.com>
2020-12-03 22:03:53 +0100

    Work on generator path.

--- app/Services/ImageGenerator/AbstractGenerator.php
+++ app/Services/ImageGenerator/AbstractGenerator.php
@@ -2,95 +2,95 @@
 
 namespace App\Services\ImageGenerator;
 
-use RuntimeException;
-
+use App\Services\ImageGenerator\Contracts\Generator as Contract;
 use Illuminate\Config\Repository;
 use Illuminate\Support\Str;
-
-use App\Services\ImageGenerator\Contracts\Generator as Contract;
+use RuntimeException;
 
 /**
  * Abstract image generator.
  *
- * @author Kacper Pruszyński (plumthedev) <kacper.pruszysnki99@gmail.com>
+ * @author  Kacper Pruszyński (plumthedev) <kacper.pruszysnki99@gmail.com>
  * @version 1.0.0
  */
 abstract class AbstractGenerator implements Contract
 {
-	/**
-	 * Default image filename length.
-	 *
-	 * @var int
-	 */
-	const FILENAME_LENGTH = 32;
+    /**
+     * Default image filename length.
+     *
+     * @var int
+     */
+    const FILENAME_LENGTH = 32;
 
-	/**
-	 * Configuration repository.
-	 *
-	 * @var \Illuminate\Config\Repository
-	 */
-	protected $config;
+    /**
+     * Configuration repository.
+     *
+     * @var \Illuminate\Config\Repository
+     */
+    protected $config;
 
-	/**
-	 * Create new instance of provider.
-	 *
-	 * @param \Illuminate\Config\Repository $config
-	 */
-	public function __construct(Repository $config)
-	{
-		$this->config = $config;
-	}
+    /**
+     * Create new instance of provider.
+     *
+     * @param \Illuminate\Config\Repository $config
+     */
+    public function __construct(Repository $config)
+    {
+        $this->config = $config;
+    }
 
-	/**
-	 * Generate image filename.
-	 *
-	 * @return string
-	 */
-	protected function generateFilename(): string
-	{
-		$filename = Str::random(
-			$this->config->get('image.filename_length', self::FILENAME_LENGTH)
-		);
+    /**
+     * Generate image filename.
+     *
+     * @return string
+     */
+    protected function generateFilename(): string
+    {
+        $filename = Str::random(
+            $this->config->get('image.filename_length', self::FILENAME_LENGTH)
+        );
 
-		return sprintf("%s.jpg", $filename);
-	}
+        return sprintf("%s.jpg", $filename);
+    }
 
-	/**
-	 * Download image by url to temp directory.
-	 *
-	 * @param string $filename
-	 * @param string $imageUrl
-	 *
-	 * @return string
-	 */
-	protected function downloadImageToTempDirectory(string $filename, string $imageUrl): string
-	{
-		$imagePath = $this->generateTempDirectoryPath($filename);
-		copy($imageUrl, $imagePath);
+    /**
+     * Download image by url to temp directory.
+     *
+     * @param string $filename
+     * @param string $imageUrl
+     *
+     * @return string
+     */
+    protected function downloadImageToTempDirectory(string $filename, string $imageUrl): string
+    {
+        $path = $this->generateTempDirectoryPath($filename);
+        $contents = file_get_contents($imageUrl);
+        $image = fopen($path, 'w+');
 
-		return $imagePath;
-	}
+        fwrite($image, $contents);
+        fclose($image);
 
-	/**
-	 * Generate temp directory path.
-	 *
-	 * @param string $filename
-	 *
-	 * @return string
-	 */
-	protected function generateTempDirectoryPath(string $filename): string
-	{
-		$tempDirectoryPath = tempnam(
-			sys_get_temp_dir(),
-			$filename
-		);
+        return $path;
+    }
 
-		if (empty($tempDirectoryPath)) {
-			throw new RuntimeException(
-				sprintf('Cannot generate temp name for image.')
-			);
-		}
+    /**
+     * Generate temp directory path.
+     *
+     * @param string $filename
+     *
+     * @return string
+     */
+    protected function generateTempDirectoryPath(string $filename): string
+    {
+        $tempDirectoryPath = sys_get_temp_dir();
+        $tempDirectoryPath = "{$tempDirectoryPath}/{$filename}";
 
-		return $tempDirectoryPath;
-	}
+        if (empty($tempDirectoryPath)) {
+            throw new RuntimeException(
+                sprintf('Cannot generate temp name for image.')
+            );
+        }
+
+        return $tempDirectoryPath;
+    }
 }
