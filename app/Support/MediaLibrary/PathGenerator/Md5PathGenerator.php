21b3d0d31bff0d02bbbc4dd32b9a281661f0ca36
plumthedev <kacper.pruszynski99@gmail.com>
2020-12-03 23:22:54 +0100

    Support media fallback when generating media path.

--- app/Support/MediaLibrary/PathGenerator/Md5PathGenerator.php
+++ app/Support/MediaLibrary/PathGenerator/Md5PathGenerator.php
@@ -2,47 +2,52 @@
 
 namespace App\Support\MediaLibrary\PathGenerator;
 
+use App\Support\MediaLibrary\Contracts\Fallback;
 use Spatie\MediaLibrary\Models\Media;
 use Spatie\MediaLibrary\PathGenerator\PathGenerator;
 
 class Md5PathGenerator implements PathGenerator
 {
-	/**
-	 * Get base path.
-	 *
-	 * @param \Spatie\MediaLibrary\Models\Media $media
-	 *
-	 * @return string
-	 */
-	public function getPath(Media $media) : string
-	{
-		$uniqueMediaKey = config('medialibrary.key');
-		$uniqueMediaKey = "{$uniqueMediaKey}|{$media->id}";
+    /**
+     * Get base path.
+     *
+     * @param \Spatie\MediaLibrary\Models\Media $media
+     *
+     * @return string
+     */
+    public function getPath(Media $media) : string
+    {
+        $uniqueMediaKey = config('medialibrary.key');
+        $uniqueMediaKey = "{$uniqueMediaKey}|{$media->id}";
 
-		return md5($uniqueMediaKey).'/';
-	}
+        if ($media instanceof Fallback) {
+            return $media->getDirectoryPath();
+        }
 
-	/**
-	 * Get path for conversions.
-	 *
-	 * @param \Spatie\MediaLibrary\Models\Media $media
-	 *
-	 * @return string
-	 */
-	public function getPathForConversions(Media $media) : string
-	{
-		return $this->getPath($media).'c/';
-	}
+        return md5($uniqueMediaKey) . '/';
+    }
 
-	/**
-	 * Get path for responsive images.
-	 *
-	 * @param \Spatie\MediaLibrary\Models\Media $media
-	 *
-	 * @return string
-	 */
-	public function getPathForResponsiveImages(Media $media): string
-	{
-		return $this->getPath($media).'/cri/';
-	}
+    /**
+     * Get path for conversions.
+     *
+     * @param \Spatie\MediaLibrary\Models\Media $media
+     *
+     * @return string
+     */
+    public function getPathForConversions(Media $media) : string
+    {
+        return $this->getPath($media).'c/';
+    }
+
+    /**
+     * Get path for responsive images.
+     *
+     * @param \Spatie\MediaLibrary\Models\Media $media
+     *
+     * @return string
+     */
+    public function getPathForResponsiveImages(Media $media): string
+    {
+        return $this->getPath($media).'/cri/';
+    }
 }
