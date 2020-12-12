f1108f4265f5c7a156bdef29b7aa9fac95b75791
plumthedev <kacper.pruszynski99@gmail.com>
2020-12-12 18:53:45 +0100

    Create web translations.

---
+++ resources/lang/pl/web.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'pages' => [
+        'homepage'         => 'Strona główna',
+        'cinematographies' => 'Filmy i seriale',
+        'my_account'       => 'Moje konto',
+    ],
+    'views' => [
+        'components' => [
+            'navbar' => [
+                'search' => [
+                    'label'       => 'Wyszukiwarka',
+                    'placeholder' => 'Co dzisiaj oglądamy?',
+                ],
+            ],
+        ],
+    ],
+];
