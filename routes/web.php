fb2e3e7884be99f31242910b247ff952c02262ce
plumthedev <kacper.pruszynski99@gmail.com>
2020-12-12 18:54:05 +0100

    Work on web routes.

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\Web\HomeController::class, 'index']);
+Route::get('/', [\App\Http\Controllers\Web\HomeController::class, 'index'])->name('home');
+
+//Auth::routes();
