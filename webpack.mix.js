c8afd7e5952084857bb52972b7046dc1d248e8f1
plumthedev <kacper.pruszynski99@gmail.com>
2020-12-13 19:56:53 +0100

    Add copy images to mix, fix web javascripts.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,13 @@
 const mix = require('laravel-mix');
 
 // javascript
-mix.js('resources/js/app.js', 'public/build/web/js')
+mix.js('resources/js/web/app.js', 'public/build/web/js')
 
 // stylesheet
 mix.sass('resources/sass/web/app.scss', 'public/build/web/css')
+
+// images
+mix.copy('resources/images/web/**/**', 'public/build/web/images');
 
 mix.options({
     processCssUrls: false
